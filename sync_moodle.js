89bde4f7ad875c684f15adf106ded9621e256560
Tombombadil7 <kirznernir@gmail.com>
2026-04-17 20:15:42 +0300

    Add '- תרגול -' to ignored phrases list

--- sync_moodle.js
+++ sync_moodle.js
@@ -38,7 +38,7 @@ const CONFIG = {
         "0840641": "תכן וייצור",
         "0440098": "חשמל לתעופה"
     },
-    ignored_phrases: ["לזום", "שעת קבלה", "זום", "Zoom", "ZOOM", "zoom","technion.zoom.us","הרצאה"],
+    ignored_phrases: ["לזום", "שעת קבלה", "זום", "Zoom", "ZOOM", "zoom","technion.zoom.us","- תרגול -","הרצאה"],
     gh_ical_path: "calendar.ics",
     gh_state_path: "todoist_state.json"
 };
